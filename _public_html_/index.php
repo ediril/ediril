9111996557bb084941e9e0f088b8c4994c535aca
Emrah Diril <emrah@diril.org>
2025-06-18 01:35:23 -0400

    Reorder

--- _public_html_/index.php
+++ _public_html_/index.php
@@ -191,14 +191,14 @@ record_visit();
 		<li><a href="https://founderfodder.com">FounderFodder</a>
 			<p>Inspiration for founders, makers and tinkerers</p>
 		</li>
-		<li><a href="https://emrahdiril.com/scrubbler">Scrubbler</a>
-			<p>A special video player that makes it easier to learn piano pieces via synesthesia videos on YouTube</p>
-		</li>
-		<li><a href="https://banalytiq.com">Banalytiq</a>
-			<p>A very <s>simple</s> banal server-side web analytics library built on sqlite and geolite2</p>
-		</li>
 		<li><a href="https://reddswan.com">ReddSwan</a>
 			<p>A new AI-augmented email client with a fresh take on UI/UX. Get the most out of your emails</p>
+		</li>
+		<li><a href="https://banalytiq.com">Banalytiq</a>
+			<p>The simplest, most banal yet sufficient server-side alternative to Google Analytics</p>
+		</li>
+		<li><a href="https://emrahdiril.com/scrubbler">Scrubbler</a>
+			<p>A special video player that makes it easier to learn piano pieces via synesthesia videos on YouTube</p>
 		</li>
 		<li><img src="_img/instagram.svg" alt="Instagram" width="24"><a href="https://instagram.com/dream_vistas.art">@dream_vistas.art</a>
 			<p>"The Dreamtress creates surreal, natured-inspired dreams infused with psychotropic wonder for all"</p>
